6a8c0ab28c0f83d5b0b35b3e98858111b9d30d8c
Šimon Kocúrek <15381356+SimonKocurek@users.noreply.github.com>
2025-12-01 11:21:32 +0100

    Day 1 Bonus

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,14 +1,18 @@
+private const val START_ROTATION = 50
+private const val MAX_ROTATION = 100
+private const val DEBUG = false
+
 fun main() {
     fun part1(input: List<String>): Int {
-        var rotation = 50
+        var rotation = START_ROTATION
         var zeroRotations = 0
 
         input
             .map { line -> line.first() to line.substring(1).toInt() }
             .onEach { (direction, shift) ->
                 rotation = when (direction) {
-                    'L' -> (rotation - (shift % 100)).let { if (it < 0) it + 100 else it }
-                    'R' -> (rotation + shift) % 100
+                    'L' -> (rotation - (shift % MAX_ROTATION)).let { if (it < 0) it + MAX_ROTATION else it }
+                    'R' -> (rotation + shift) % MAX_ROTATION
                     else -> error("Unknown direction: $direction")
                 }
 
@@ -21,11 +25,58 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var rotation = START_ROTATION
+        var zeroRotations = 0
+
+        input
+            .map { line -> line.first() to line.substring(1).toInt() }
+            .onEach { (direction, shift) ->
+                if (DEBUG) {
+                    println("Rotation $rotation")
+                }
+
+                when (direction) {
+                    'L' -> {
+                        zeroRotations += shift / MAX_ROTATION
+                        var newRotation = rotation - (shift % MAX_ROTATION)
+                        if (newRotation < 0) {
+                            newRotation += MAX_ROTATION
+
+                            if (rotation != 0) {
+                                zeroRotations++
+                            }
+                        }
+                        if (newRotation == 0) {
+                            zeroRotations++
+                        }
+
+                        rotation = newRotation
+                    }
+
+                    'R' -> {
+                        rotation += shift
+                        zeroRotations += rotation / MAX_ROTATION
+                        rotation %= MAX_ROTATION
+                    }
+
+                    else -> error("Unknown direction: $direction")
+                }
+
+                if (DEBUG) {
+                    println("Direction $direction")
+                    println("Shift $shift")
+                    println("Updated Rotation $rotation")
+                    println("Zero Rotations $zeroRotations")
+                    println()
+                }
+            }
+
+        return zeroRotations
     }
 
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
     val input = readInput("Day01")
     part1(input).println()
