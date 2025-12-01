684a310890e575576fd1f109afa725aed5dd7271
Šimon Kocúrek <15381356+SimonKocurek@users.noreply.github.com>
2025-12-01 10:55:37 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,32 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var rotation = 50
+        var zeroRotations = 0
+
+        input
+            .map { line -> line.first() to line.substring(1).toInt() }
+            .onEach { (direction, shift) ->
+                rotation = when (direction) {
+                    'L' -> (rotation - (shift % 100)).let { if (it < 0) it + 100 else it }
+                    'R' -> (rotation + shift) % 100
+                    else -> error("Unknown direction: $direction")
+                }
+
+                if (rotation == 0) {
+                    zeroRotations++
+                }
+            }
+
+        return zeroRotations
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
