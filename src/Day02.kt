c92335ccc6e320dc78c48211b5aadb0d610b3ea9
Šimon Kocúrek <15381356+SimonKocurek@users.noreply.github.com>
2025-12-02 13:55:48 +0100

    Day 2 Bonus

--- src/Day02.kt
+++ src/Day02.kt
@@ -17,13 +17,41 @@ fun main() {
             }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        return input
+            .first() // Input is a single line
+            .split(",")
+            .map { range -> range.split("-").map { it.toLong() } }
+            .map { (start, end) -> start..end }
+            .sumOf { range ->
+                range
+                    .map { it.toString() }
+                    .filter {
+                        (1..it.length / 2).any { substringLength ->
+                            if (it.length % substringLength != 0) {
+                                return@any false // Micro-optimization - substring can't repeat until the end, if the length is not multiple of substring length
+                            }
+
+                            val baseSubstring = it.substring(0, substringLength)
+
+                            var offset = substringLength
+                            while (offset < it.length) {
+                                if (baseSubstring != it.substring(offset, offset + substringLength)) {
+                                    return@any false
+                                }
+                                offset += substringLength
+                            }
+
+                            return@any true
+                        }
+                    }
+                    .sumOf { it.toLong() }
+            }
     }
 
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 1_227_775_554L)
-//    check(part2(testInput) == 6)
+    check(part2(testInput) == 4_174_379_265L)
 
     val input = readInput("Day02")
     part1(input).println()
