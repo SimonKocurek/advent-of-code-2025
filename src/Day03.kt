32d62f2b2c89ac0daab40c306b21c54d1de08d18
Šimon Kocúrek <15381356+SimonKocurek@users.noreply.github.com>
2025-12-03 12:38:23 +0100

    Day 3 Bonus

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,3 +1,8 @@
+import java.math.BigInteger
+import java.util.Stack
+
+private const val BATTERIES = 12
+
 fun main() {
     fun part1(input: List<String>): Int {
         return input
@@ -21,13 +26,42 @@ fun main() {
             }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): BigInteger {
+
+        fun turnOnBatteries(line: String, offset: Int, batteries: MutableList<Int>): List<Int>? {
+            if (batteries.size == BATTERIES) {
+                return batteries.toList() // We have turned on all batteries
+            }
+
+            return (9 downTo 1).firstNotNullOfOrNull { digit ->
+                val foundAt = line.indexOf(digit.digitToChar(), startIndex = offset)
+                if (foundAt == -1) {
+                    return@firstNotNullOfOrNull null // Digit not found
+                }
+                val remainingBatteries = BATTERIES - batteries.size - 1 // Including just found battery one
+                if (foundAt > line.lastIndex - remainingBatteries) {
+                    return@firstNotNullOfOrNull  null // Not enough room for remaining batteries
+                }
+
+                batteries.add(digit)
+                val result = turnOnBatteries(line, offset = foundAt + 1, batteries = batteries)
+                batteries.removeAt(batteries.lastIndex)
+
+                result
+            }
+        }
+
+        return input.sumOf { line ->
+            turnOnBatteries(line, offset = 0, batteries = mutableListOf())
+                ?.joinToString("")
+                ?.toBigInteger()
+                ?: error("Could not turn on all batteries for line: $line")
+        }
     }
 
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 357)
-//    check(part2(testInput) == 1_174_379_265)
+    check(part2(testInput) == BigInteger("3121910778619"))
 
     val input = readInput("Day03")
     part1(input).println()
