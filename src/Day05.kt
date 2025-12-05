83d703d87937d195d179ac643430bc6b681d6564
Šimon Kocúrek <15381356+SimonKocurek@users.noreply.github.com>
2025-12-05 11:01:19 +0100

    Day 5 Bonus

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,4 +1,5 @@
 import java.math.BigInteger
+import java.math.BigInteger.ONE
 
 fun main() {
     fun part1(input: List<String>): Int {
@@ -14,13 +15,42 @@ fun main() {
             .count { id -> ranges.any { (start, end) -> id in start..end } }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): BigInteger {
+        val ranges = input
+            .takeWhile { it.isNotEmpty() }
+            .map { line -> line.split("-").map { BigInteger(it) } }
+            .sortedBy { (start, _) -> start }
+
+        val mergedRanges = buildList {
+            ranges.forEach { (start, end) ->
+                if (isEmpty()) {
+                    // Should happen only in the first iteration
+                    add(start to end)
+                    return@forEach
+                }
+
+                if (start > last().second) {
+                    // No overlap
+                    add(start to end)
+                    return@forEach
+                }
+
+                if (end <= last().second) {
+                    // Fully contained
+                    return@forEach
+                }
+
+                val lastRange = pop()
+                add(lastRange.first to end)
+            }
+        }
+
+        return mergedRanges.sumOf { (start, end) -> end - start + ONE }
     }
 
     val testInput = readInput("Day05_test")
     check(part1(testInput) == 3)
-//    check(part2(testInput) == 43)
+    check(part2(testInput) == BigInteger("14"))
 
     val input = readInput("Day05")
     part1(input).println()
