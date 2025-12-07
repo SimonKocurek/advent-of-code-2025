a2017f41028f787d786bb5f712ab476572efd444
Šimon Kocúrek <15381356+SimonKocurek@users.noreply.github.com>
2025-12-07 11:26:00 +0100

    Day 7 Bonus

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,3 +1,5 @@
+import java.math.BigInteger
+
 fun main() {
     fun part1(input: List<String>): Int {
         val xSize = input[0].length
@@ -38,13 +40,43 @@ fun main() {
         return splits
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): BigInteger {
+        val xSize = input[0].length
+
+        var enabled = Array<BigInteger>(xSize) { BigInteger.ZERO }
+        var enabledNext = Array<BigInteger>(xSize) { BigInteger.ZERO }
+
+        val startX = input[0].indexOf('S')
+        enabled[startX] = BigInteger.ONE
+
+        for (y in 1..input.lastIndex) {
+            for (x in 0 until xSize) {
+
+                when (input[y][x]) {
+                    '^' -> {
+                        if (x - 1 >= 0) {
+                            enabledNext[x - 1] += enabled[x]
+                        }
+                        if (x + 1 < xSize) {
+                            enabledNext[x + 1] += enabled[x]
+                        }
+                    }
+                    '.' -> enabledNext[x] += enabled[x]
+                }
+            }
+
+            val temp = enabled
+            enabled = enabledNext
+            enabledNext = temp
+            enabledNext.fill(BigInteger.ZERO)
+        }
+
+        return enabled.sumOf { it }
     }
 
     val testInput = readInput("Day07_test")
     check(part1(testInput) == 21)
-//    check(part2(testInput) == BigInteger("3263827"))
+    check(part2(testInput) == BigInteger("40"))
 
     val input = readInput("Day07")
     part1(input).println()
